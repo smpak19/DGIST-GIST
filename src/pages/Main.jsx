b66f48ef72964c0e8d3a9dc4f52207f7225ed75c
Jinnam <smpak@kaist.ac.kr>
2022-01-13 21:40:01 +0900

    Main

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import {Link} from "react-router-dom"
+import './main.css'
 
 function Main() {
     return (
         <div className="Main">
-            <h1>
-                This is Main Screen
-            </h1>
-            <hr/>
-            <Link to ="/login">
-            <button>시작하기</button>
-            </Link>
+             <header className ="Header">
+                <h1>Madcamp week 3</h1>
+            </header>
+
+            <div className= "Body">
+                <h1>DGIST-UNIST-GIST</h1>
+                <Link to ="/login">
+                    <button><h3>시작하기</h3></button>
+                </Link> 
+            </div>
+
+            <footer className= "Footer">
+               <h4>
+                <p>Made by Junyoung, Junseo, Seungmin</p>   
+                </h4> 
+            </footer>
         </div>
     )
 }
