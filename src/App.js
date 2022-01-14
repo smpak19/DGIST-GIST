f7935bc75be6e3ec88fb5714a523c6988aad105d
DDoubleA <ace9804@unist.ac.kr>
2022-01-14 12:59:04 +0900

    courses

--- src/App.js
+++ src/App.js
@@ -2,15 +2,12 @@ import './App.css';
 import {BrowserRouter, Route, Routes, Link} from 'react-router-dom'
 import Login from './pages/Login'
 import Main from './pages/Main'
+import Courses from './pages/Courses'
 
 function App() {
   return (
     <div className="App">
       <main className = "App-main">
-        <Routes>
-          <Route path = "/" element = { <Main/>} />
-          <Route path = "/login" element= {<Login/>} /> 
-        </Routes>
         <hr/>
         <Link to ="/login">
           <button>Login</button>
@@ -18,6 +15,14 @@ function App() {
         <Link to="/">
           <button>Main</button>
         </Link>
+        <Link to ="/courses">
+          <button>courses</button>
+        </Link>
+        <Routes>
+          <Route path = "/" element = { <Main/>} />
+          <Route path = "/login" element= {<Login/>} /> 
+          <Route path = "/courses" element= {<Courses/>} /> 
+        </Routes>
       </main>
     </div>
   );
