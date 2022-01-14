8b6a3e7a10d7b9cad58fc9f9a0ad171fd8fd1b21
Jinnam <smpak@kaist.ac.kr>
2022-01-14 20:52:21 +0900

    logic

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,26 +1,33 @@
 
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import CourseInfo from "../Component/CourseInfo";
+import data from "./until19.json"
+import {useNavigate, useLocation} from 'react-router-dom'
 
 function Courses() {
     // const [courses, setcourse] = useState([{courseId : 'cse496', courseName : '몰입캠프'}, {courseId : 'cse123', courseName : '데이터 구조'}, {courseId : 'cse234', courseName : '운영체제'}])
     // 전체 과목
-    const course = [{courseId : 'cse496', courseName : '몰입캠프', credit: 3}, {courseId : 'cse123', courseName : '데이터 구조', credit: 3}, {courseId : 'cse234', courseName : '운영체제', credit: 3}]
+    const course = data
     // 사용자가 들은 과목
-    const [isheard, setisheard] = useState(['cse496', 'cse123',])
+    const [isheard, setisheard] = useState([])
+    const nav = useNavigate()
+    const loc = useLocation()
+
 
     function handleSubmit(e) {
         e.preventDefault();
         console.log('You clicked submit.!!!');
         console.log(isheard)
+        nav("/result", {state: {arr: isheard, num: loc.state}})
       }
+
 
     return (
         
         <div>
             <div>{ course.map(e => (<CourseInfo course = {e} taken = {isheard} settaken = {setisheard} />)) } </div>
             <form onSubmit={handleSubmit}>
-                <button onclick={() => handleSubmit}> Activate Lasers </button>
+                <button onClick={() => handleSubmit}>제출하기 </button>
             </form>
         </div>
         
